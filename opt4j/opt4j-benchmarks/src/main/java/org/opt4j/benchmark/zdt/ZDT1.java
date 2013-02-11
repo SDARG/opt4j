a2dde170d1dc3285b8c6513d8e93a26921af1356
Felix Reimann <felix@fex-it.de>
2013-02-11 20:34:12 +0100

    rm unused import

--- opt4j/opt4j-benchmarks/src/main/java/org/opt4j/benchmark/zdt/ZDT1.java
+++ opt4j/opt4j-benchmarks/src/main/java/org/opt4j/benchmark/zdt/ZDT1.java
@@ -1,16 +1,13 @@
 /**
- * Opt4J is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
+ * Opt4J is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * Opt4J is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
+ * Opt4J is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with Opt4J. If not, see http://www.gnu.org/licenses/. 
+ * You should have received a copy of the GNU Lesser General Public License along with Opt4J. If not, see
+ * http://www.gnu.org/licenses/.
  */
 
 package org.opt4j.benchmark.zdt;
@@ -22,7 +19,6 @@ import java.util.List;
 
 import org.opt4j.benchmark.DoubleString;
 import org.opt4j.core.Phenotype;
-import org.opt4j.genotype.BooleanGenotype;
 
 /**
  * Function ZDT 1.
@@ -89,8 +85,7 @@ public class ZDT1 extends ZDTEvaluator<DoubleString> {
 	}
 
 	/**
-	 * Creates a list of doubles from an arbitrary genotype (
-	 * {@code DoubleVectorPhenotype} or {@code BooleanGenotype}).
+	 * Creates a list of doubles from an arbitrary genotype ( {@code DoubleVectorPhenotype} or {@code BooleanGenotype}).
 	 * 
 	 * @param phenotype
 	 *            the phenotype
