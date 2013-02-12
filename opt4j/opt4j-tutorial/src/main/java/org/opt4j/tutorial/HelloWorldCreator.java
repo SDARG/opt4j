0316e24cbc5b051e7b01c89ee60d9b512605afb7
Martin Lukasiewycz <lukasiewycz@gmail.com>
2013-02-12 12:55:36 +0800

    Simplify HelloWorld tutorial code

--- opt4j/opt4j-tutorial/src/main/java/org/opt4j/tutorial/HelloWorldCreator.java
+++ opt4j/opt4j-tutorial/src/main/java/org/opt4j/tutorial/HelloWorldCreator.java
@@ -1,7 +1,5 @@
 package org.opt4j.tutorial;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import org.opt4j.core.problem.Creator;
@@ -9,18 +7,13 @@ import org.opt4j.genotype.SelectGenotype;
 
 public class HelloWorldCreator implements Creator<SelectGenotype<Character>> {
 
-	List<Character> chars = new ArrayList<Character>();
-	{
-		for (char c = 'A'; c <= 'Z'; c++) {
-			chars.add(c);
-		}
-		chars.add(' ');
-	}
+	Character[] ALPHABET = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+			'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
 	Random random = new Random();
 
 	@Override
 	public SelectGenotype<Character> create() {
-		SelectGenotype<Character> genotype = new SelectGenotype<Character>(chars);
+		SelectGenotype<Character> genotype = new SelectGenotype<Character>(ALPHABET);
 		genotype.init(random, 11);
 		return genotype;
 	}
--- opt4j/opt4j-tutorial/src/main/java/org/opt4j/tutorial/HelloWorldEvaluator.java
+++ opt4j/opt4j-tutorial/src/main/java/org/opt4j/tutorial/HelloWorldEvaluator.java
@@ -10,22 +10,17 @@ public class HelloWorldEvaluator implements Evaluator<PhenotypeWrapper<String>> 
 
 	Objective objective = new Objective("objective", Sign.MAX);
 
-	String target = "HELLO WORLD";
-
 	@Override
 	public Objectives evaluate(PhenotypeWrapper<String> phenotype) {
 		String s = phenotype.get();
 
 		int value = 0;
 		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == target.charAt(i)) {
-				value++;
-			}
+			value += (s.charAt(i) == "HELLO WORLD".charAt(i)) ? 1 : 0;
 		}
 
 		Objectives objectives = new Objectives();
 		objectives.add(objective, value);
 		return objectives;
 	}
-
 }
