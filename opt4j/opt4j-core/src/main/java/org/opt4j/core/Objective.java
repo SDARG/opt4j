ffc210b1867fe9da7de86d43e2151f44b0297596
Martin Lukasiewycz <lukasiewycz@gmail.com>
2013-02-19 12:26:36 +0800

    Add hashCode and equals to Objective class

--- opt4j/opt4j-core/src/main/java/org/opt4j/core/Objective.java
+++ opt4j/opt4j-core/src/main/java/org/opt4j/core/Objective.java
@@ -131,4 +131,42 @@ public class Objective implements Comparable<Objective> {
 		return getName() + "(" + getSign() + ")";
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sign == null) ? 0 : sign.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Objective other = (Objective) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sign != other.sign)
+			return false;
+		return true;
+	}
+
 }
