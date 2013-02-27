9f9b8d6b88627b43e08e8efe35f9cd03243649d8
Felix Reimann <felix@fex-it.de>
2013-02-27 12:46:27 +0100

    update optimizer package level javadoc

--- opt4j/opt4j-core/src/main/java/org/opt4j/core/optimizer/package-info.java
+++ opt4j/opt4j-core/src/main/java/org/opt4j/core/optimizer/package-info.java
@@ -24,44 +24,45 @@
  * {@link org.opt4j.core.optimizer.IterativeOptimizer}.
  * </p>
  * <p>
- * The {@link org.opt4j.core.optimizer.Optimizer} manipulates the
- * {@link org.opt4j.core.optimizer.Population} and
- * {@link org.opt4j.core.optimizer.Archive}. The
- * {@link org.opt4j.core.optimizer.Population} contains the
- * {@link org.opt4j.core.Individual}s of the current
- * {@link org.opt4j.core.optimizer.Iteration}, while the
- * {@link org.opt4j.core.optimizer.Archive} contains the non-dominated
- * {@link org.opt4j.core.Individual}s that were obtained throughout the
- * optimization process.
- * </p>
- * <p>
- * The {@link org.opt4j.core.optimizer.Optimizer} might use any
- * {@link org.opt4j.core.optimizer.Operator} to vary the
- * {@link org.opt4j.core.Genotype} and use the
+ * The {@link org.opt4j.core.optimizer.IterativeOptimizer} initializes the
+ * {@link org.opt4j.core.optimizer.Population} by using the
  * {@link org.opt4j.core.IndividualFactory} to get new
- * {@link org.opt4j.core.Individual}s. Note that instead using the
- * {@link org.opt4j.core.problem.Decoder} and
- * {@link org.opt4j.core.problem.Evaluator} directly, the
- * {@link org.opt4j.core.optimizer.Optimizer} uses an
- * {@link org.opt4j.core.optimizer.IndividualCompleter} to determine the
- * phenotype and {@link org.opt4j.core.Objectives} of a set of
- * {@link org.opt4j.core.Individual}s.
+ * {@link org.opt4j.core.Individual}s. In each iteration, the
+ * {@link org.opt4j.core.optimizer.IterativeOptimizer} uses
+ * {@link org.opt4j.core.optimizer.Operator}s to vary the
+ * {@link org.opt4j.core.Genotype}s. The package {@code org.opt4j.operators}
+ * contains predefined {@link org.opt4j.core.optimizer.Operator} classes which
+ * are applicable for different {@link org.opt4j.core.Genotype}s.
  * </p>
  * <p>
- * The {@link org.opt4j.core.optimizer.AbstractOptimizer} implements some
- * essential methods for coupling optimizers with the framework. It interacts
- * with the {@link org.opt4j.core.optimizer.OptimizerIterationListener}s, the
- * {@link org.opt4j.core.optimizer.OptimizerStateListener}s, and the
+ * The {@link org.opt4j.core.optimizer.IterativeOptimizer} is managed by the
+ * {@link org.opt4j.core.optimizer.OptimizationMediator} which itself is an
+ * {@link org.opt4j.core.optimizer.AbstractOptimizer}. The
+ * {@link org.opt4j.core.optimizer.AbstractOptimizer} implements some essential
+ * methods for coupling optimizers with the framework. It interacts with the
+ * {@link org.opt4j.core.optimizer.OptimizerIterationListener}s which are
+ * informed whenever an iteration is complete, the
+ * {@link org.opt4j.core.optimizer.OptimizerStateListener}s which are informed
+ * whenever the optimization starts or stops, and the
  * {@link org.opt4j.core.optimizer.Control}: The
  * {@link org.opt4j.core.optimizer.Control} allows to pause and resume, stop, or
- * terminate the optimization process. Moreover, it updates the
- * {@link org.opt4j.core.optimizer.Archive} with the current
- * {@link org.opt4j.core.optimizer.Population}. It triggers the
- * {@link org.opt4j.core.optimizer.Iteration} count as well as with the
- * {@link org.opt4j.core.optimizer.IndividualCompleter}. The
- * {@link org.opt4j.core.optimizer.OptimizationMediator} is an
- * {@link org.opt4j.core.optimizer.AbstractOptimizer} which handles the
- * {@link org.opt4j.core.optimizer.IterativeOptimizer}.
+ * terminate the optimization process. While the
+ * {@link org.opt4j.core.optimizer.Population} contains the
+ * {@link org.opt4j.core.Individual}s of the current
+ * {@link org.opt4j.core.optimizer.Iteration}, the
+ * {@link org.opt4j.core.optimizer.Archive} contains the non-dominated
+ * {@link org.opt4j.core.Individual}s that were obtained throughout the whole
+ * optimization process. The {@link org.opt4j.core.optimizer.AbstractOptimizer}
+ * updates the {@link org.opt4j.core.optimizer.Archive} with the current
+ * {@link org.opt4j.core.optimizer.Population} and triggers the
+ * {@link org.opt4j.core.optimizer.Iteration} count. Instead of using the
+ * {@link org.opt4j.core.problem.Decoder} and
+ * {@link org.opt4j.core.problem.Evaluator} directly, the
+ * {@link org.opt4j.core.optimizer.IndividualCompleter} is used by
+ * {@link org.opt4j.core.optimizer.Optimizer}s to determine the phenotype and
+ * the {@link org.opt4j.core.Objectives} of new
+ * {@link org.opt4j.core.Individual}s in the
+ * {@link org.opt4j.core.optimizer.Population}.
  * </p>
  */
 package org.opt4j.core.optimizer;
