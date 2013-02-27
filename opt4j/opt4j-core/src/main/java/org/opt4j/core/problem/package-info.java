ab439b5401b4131af116865997ab16031d4145ab
Felix Reimann <felix@fex-it.de>
2013-02-27 12:56:22 +0100

    update problem javadoc

--- opt4j/opt4j-core/src/main/java/org/opt4j/core/problem/package-info.java
+++ opt4j/opt4j-core/src/main/java/org/opt4j/core/problem/package-info.java
@@ -18,7 +18,7 @@
  * Provides the classes for the optimization problem. To define a new
  * optimization problem, the {@link org.opt4j.core.problem.Creator}, the
  * {@link org.opt4j.core.problem.Decoder}, and the
- * {@link org.opt4j.core.problem.Evaluator} must be defined.
+ * {@link org.opt4j.core.problem.Evaluator} must be implemented.
  * </p>
  * <dl>
  * <dt>{@link org.opt4j.core.problem.Creator}</dt>
@@ -27,11 +27,11 @@
  * random {@link org.opt4j.core.Genotype}s. It is typically used at the
  * beginning of an optimization by the
  * {@link org.opt4j.core.optimizer.Optimizer} to create the initial
- * {@link org.opt4j.core.optimizer.Population}. The size of the
+ * {@link org.opt4j.core.optimizer.Population}. The
  * {@link org.opt4j.core.Genotype}s, the {@link org.opt4j.core.problem.Creator}
  * creates, define the search space for the
  * {@link org.opt4j.core.optimizer.Optimizer}. The package
- * {@code org.opt4j.genotype} contains predefined
+ * {@code org.opt4j.core.genotype} contains predefined
  * {@link org.opt4j.core.Genotype} classes that allow a modular assembly. Thus,
  * usually you do not have to write your custom {@link org.opt4j.core.Genotype}.
  * </dd>
@@ -43,21 +43,28 @@
  * 
  * <dt>{@link org.opt4j.core.problem.Evaluator}</dt>
  * <dd>The task of the {@link org.opt4j.core.problem.Evaluator} is the
- * evaluation of phenotypes to the {@link org.opt4j.core.Objectives}. This is
+ * evaluation of phenotypes to the {@link org.opt4j.core.Objectives}. These are
  * typically used by the {@link org.opt4j.core.optimizer.Optimizer} to determine
  * the fitness of the {@link org.opt4j.core.Individual} or by the
  * {@link org.opt4j.core.optimizer.Archive} to determine if the
  * {@link org.opt4j.core.Individual} Pareto-dominates another one.</dd>
  * </dl>
  * <p>
- * Additional information about the problem should be contained in an user
- * defined class that is injected as needed.
- * </p>
- * <p>
  * Inherit from the {@link org.opt4j.core.problem.ProblemModule} to bind a
  * {@link org.opt4j.core.problem.Creator},
  * {@link org.opt4j.core.problem.Decoder}, and
  * {@link org.opt4j.core.problem.Evaluator} belonging together.
+ * </p>
+ * <p>
+ * For multi-objective optimization, one
+ * {@link org.opt4j.core.problem.Evaluator} can set several
+ * {@link org.opt4j.core.Objective}s or - to allow a separation of concerns -
+ * also several {@link org.opt4j.core.problem.Evaluator}s can be added using the
+ * {@link org.opt4j.core.problem.ProblemModule}. They are managed by the
+ * {@link org.opt4j.core.problem.MultiEvaluator}. The order in which the
+ * {@link org.opt4j.core.problem.MultiEvaluator} calls the
+ * {@link org.opt4j.core.problem.Evaluator}s can be influenced by the
+ * {@link org.opt4j.core.problem.Priority}.
  * </p>
  */
 package org.opt4j.core.problem;
