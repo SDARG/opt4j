aa6ae556e53c509ccc79192bc7d7b35c37d9a5d6
Felix Reimann <felix@fex-it.de>
2018-02-22 22:53:03 +0100

    fixed deprecation warnings

--- opt4j-core/src/main/java/org/opt4j/core/config/visualization/PropertyPanel.java
+++ opt4j-core/src/main/java/org/opt4j/core/config/visualization/PropertyPanel.java
@@ -1,25 +1,19 @@
 /*******************************************************************************
  * Copyright (c) 2014 Opt4J
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *******************************************************************************/
- 
 
 package org.opt4j.core.config.visualization;
 
@@ -55,8 +49,8 @@ import org.opt4j.core.config.Requirement;
 import org.opt4j.core.config.annotations.File;
 
 /**
- * The {@link PropertyPanel} is a panel for the configuration of one module.
- * Properties and values are arranged in a table.
+ * The {@link PropertyPanel} is a panel for the configuration of one module. Properties and values are arranged in a
+ * table.
  * 
  * @author lukasiewycz
  * 
@@ -75,8 +69,7 @@ public class PropertyPanel extends JPanel {
 	protected final Map<Property, Component> components = new HashMap<Property, Component>();
 
 	/**
-	 * Constructs a {@link PropertyPanel} for one {@link PropertyModule}
-	 * instance.
+	 * Constructs a {@link PropertyPanel} for one {@link PropertyModule} instance.
 	 * 
 	 * @param module
 	 *            the instance of the {@link PropertyModule}
@@ -112,7 +105,7 @@ public class PropertyPanel extends JPanel {
 			for (int k = 0; k < obj.length; k++) {
 				obj[k] = type.getEnumConstants()[k];
 			}
-			final JComboBox box = new JComboBox(obj);
+			final JComboBox<Object> box = new JComboBox<Object>(obj);
 			box.setSelectedItem(value);
 
 			box.addActionListener(new ActionListener() {
