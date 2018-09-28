30077d044492942018f94774013a6da791a31627
Felix Reimann <felix@fex-it.de>
2018-09-28 20:54:44 +0200

    use mock individuals

--- opt4j-core/src/test/java/org/opt4j/core/common/archive/BoundedArchiveTest.java
+++ opt4j-core/src/test/java/org/opt4j/core/common/archive/BoundedArchiveTest.java
@@ -1,18 +1,13 @@
 package org.opt4j.core.common.archive;
 
+import static org.mockito.Mockito.mock;
+
 import java.util.Collection;
 import java.util.Collections;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.opt4j.core.Individual;
-import org.opt4j.core.IndividualFactory;
-import org.opt4j.core.Objective;
-import org.opt4j.core.Objectives;
-import org.opt4j.core.common.archive.CrowdingArchiveTest.MockProblemModule;
-
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 public class BoundedArchiveTest {
 	@Test(expected = IllegalArgumentException.class)
@@ -56,17 +51,7 @@ public class BoundedArchiveTest {
 
 	@Test
 	public void addCheckedIndividual() {
-		Injector injector = Guice.createInjector(new MockProblemModule());
-		IndividualFactory factory = injector.getInstance(IndividualFactory.class);
-
-		Objective o0 = new Objective("o0");
-		Objective o1 = new Objective("o1");
-
-		Individual individual1 = factory.create();
-		Objectives objectivesA0 = new Objectives();
-		objectivesA0.add(o0, 1);
-		objectivesA0.add(o1, 0);
-		individual1.setObjectives(objectivesA0);
+		Individual individual1 = mock(Individual.class);
 
 		BoundedArchive archive = new BoundedArchive(1) {
 			@Override
@@ -82,17 +67,7 @@ public class BoundedArchiveTest {
 
 	@Test
 	public void addCheckedIndividualTwice() {
-		Injector injector = Guice.createInjector(new MockProblemModule());
-		IndividualFactory factory = injector.getInstance(IndividualFactory.class);
-
-		Objective o0 = new Objective("o0");
-		Objective o1 = new Objective("o1");
-
-		Individual individual1 = factory.create();
-		Objectives objectivesA0 = new Objectives();
-		objectivesA0.add(o0, 1);
-		objectivesA0.add(o1, 0);
-		individual1.setObjectives(objectivesA0);
+		Individual individual1 = mock(Individual.class);
 
 		BoundedArchive archive = new BoundedArchive(1) {
 			@Override
@@ -109,23 +84,8 @@ public class BoundedArchiveTest {
 
 	@Test(expected = IndexOutOfBoundsException.class)
 	public void addCheckedIndividualDespiteArchiveFull() {
-		Injector injector = Guice.createInjector(new MockProblemModule());
-		IndividualFactory factory = injector.getInstance(IndividualFactory.class);
-
-		Objective o0 = new Objective("o0");
-		Objective o1 = new Objective("o1");
-
-		Individual individual1 = factory.create();
-		Objectives objectivesA0 = new Objectives();
-		objectivesA0.add(o0, 1);
-		objectivesA0.add(o1, 0);
-		individual1.setObjectives(objectivesA0);
-
-		Individual individual2 = factory.create();
-		Objectives objectivesA1 = new Objectives();
-		objectivesA1.add(o0, 0);
-		objectivesA1.add(o1, 1);
-		individual2.setObjectives(objectivesA1);
+		Individual individual1 = mock(Individual.class);
+		Individual individual2 = mock(Individual.class);
 
 		BoundedArchive archive = new BoundedArchive(1) {
 			@Override
@@ -143,17 +103,7 @@ public class BoundedArchiveTest {
 
 	@Test
 	public void addCheckedIndividuals() {
-		Injector injector = Guice.createInjector(new MockProblemModule());
-		IndividualFactory factory = injector.getInstance(IndividualFactory.class);
-
-		Objective o0 = new Objective("o0");
-		Objective o1 = new Objective("o1");
-
-		Individual individual1 = factory.create();
-		Objectives objectivesA0 = new Objectives();
-		objectivesA0.add(o0, 1);
-		objectivesA0.add(o1, 0);
-		individual1.setObjectives(objectivesA0);
+		Individual individual1 = mock(Individual.class);
 
 		BoundedArchive archive = new BoundedArchive(1) {
 			@Override
@@ -169,23 +119,8 @@ public class BoundedArchiveTest {
 
 	@Test(expected = IndexOutOfBoundsException.class)
 	public void addCheckedIndividualsDespiteArchiveFull() {
-		Injector injector = Guice.createInjector(new MockProblemModule());
-		IndividualFactory factory = injector.getInstance(IndividualFactory.class);
-
-		Objective o0 = new Objective("o0");
-		Objective o1 = new Objective("o1");
-
-		Individual individual1 = factory.create();
-		Objectives objectivesA0 = new Objectives();
-		objectivesA0.add(o0, 1);
-		objectivesA0.add(o1, 0);
-		individual1.setObjectives(objectivesA0);
-
-		Individual individual2 = factory.create();
-		Objectives objectivesA1 = new Objectives();
-		objectivesA1.add(o0, 0);
-		objectivesA1.add(o1, 1);
-		individual2.setObjectives(objectivesA1);
+		Individual individual1 = mock(Individual.class);
+		Individual individual2 = mock(Individual.class);
 
 		BoundedArchive archive = new BoundedArchive(1) {
 			@Override
--- opt4j-core/src/test/java/org/opt4j/core/common/archive/UnboundedArchiveTest.java
+++ opt4j-core/src/test/java/org/opt4j/core/common/archive/UnboundedArchiveTest.java
@@ -1,33 +1,18 @@
 package org.opt4j.core.common.archive;
 
+import static org.mockito.Mockito.mock;
+
 import java.util.Collections;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.opt4j.core.Individual;
-import org.opt4j.core.IndividualFactory;
-import org.opt4j.core.Objective;
-import org.opt4j.core.Objectives;
-import org.opt4j.core.common.archive.CrowdingArchiveTest.MockProblemModule;
-
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 public class UnboundedArchiveTest {
 
 	@Test
 	public void updateWithNondominated() {
-		Injector injector = Guice.createInjector(new MockProblemModule());
-		IndividualFactory factory = injector.getInstance(IndividualFactory.class);
-
-		Objective o0 = new Objective("o0");
-		Objective o1 = new Objective("o1");
-
-		Individual individual = factory.create();
-		Objectives objectivesA0 = new Objectives();
-		objectivesA0.add(o0, 0);
-		objectivesA0.add(o1, 0);
-		individual.setObjectives(objectivesA0);
+		Individual individual = mock(Individual.class);
 
 		UnboundedArchive archive = new UnboundedArchive();
 		Assert.assertFalse(archive.iterator().hasNext());
@@ -38,17 +23,7 @@ public class UnboundedArchiveTest {
 
 	@Test
 	public void updateWithExisting() {
-		Injector injector = Guice.createInjector(new MockProblemModule());
-		IndividualFactory factory = injector.getInstance(IndividualFactory.class);
-
-		Objective o0 = new Objective("o0");
-		Objective o1 = new Objective("o1");
-
-		Individual individual = factory.create();
-		Objectives objectivesA0 = new Objectives();
-		objectivesA0.add(o0, 0);
-		objectivesA0.add(o1, 0);
-		individual.setObjectives(objectivesA0);
+		Individual individual = mock(Individual.class);
 
 		UnboundedArchive archive = new UnboundedArchive();
 		archive.updateWithNondominated(Collections.singleton(individual));
