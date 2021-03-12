11ef8f2a6a1cf81fa3a8d001ced87e54bc5c1280
Fedor <fedor@dps.uibk.ac.at>
2021-03-12 09:54:55 +0100

    jacocorootreport working

---
+++ opt4j-benchmarks/src/test/java/org/opt4j/benchmarks/PseudoTest.java
@@ -0,0 +1,13 @@
+package org.opt4j.benchmarks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PseudoTest {
+
+	@Test
+	public void test() {
+	}
+
+}
---
+++ opt4j-tutorial/src/test/java/prg/opt4j/operators/PseudoTest.java
@@ -0,0 +1,14 @@
+package prg.opt4j.operators;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PseudoTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
