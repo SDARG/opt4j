e4d31eb2352a21cec960cf451759be573688c875
Fedor Smirnov <fedorsmirnov89@gmail.com>
2023-11-12 10:28:02 +0100

    Version bump for Gradle and Java
    Build changes:
    - updated build syntax to work with gradle 8+
    - rebuild the gradle wrapper
    
    Dependencies:
    - added the guice dependency where appropriate
    - bumped guice version
    - bumped mockito version

--- opt4j-optimizers/src/test/java/org/opt4j/optimizers/ea/aeseh/ESamplingSurvivorGenerationBasicTest2.java
+++ opt4j-optimizers/src/test/java/org/opt4j/optimizers/ea/aeseh/ESamplingSurvivorGenerationBasicTest2.java
@@ -3,7 +3,7 @@ package org.opt4j.optimizers.ea.aeseh;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
